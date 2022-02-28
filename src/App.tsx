e0b81539c0e221b2b3e4b59b79373c1028839056
josetr <37419832+josetr@users.noreply.github.com>
2022-02-28 21:24:12 +0000

    Changes to deploy to a subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename="f1">
       <header className="app-header">
         <nav className="app-nav">
           <ul>
